c70bc4074bfe917de6f638cda876fac2cfcad534
Cornelius Lilge <cornelius.lilge@gmail.com>
2014-07-20 22:35:14 +0200

    Cleaned imports

--- app/controllers/AtomFeedController.scala
+++ app/controllers/AtomFeedController.scala
@@ -1,27 +1,11 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
 import views._
-import play.api.db.slick.DBAction
-import models._
+import models.FeedConfig
 import service.PostService
-import play.api.db.slick._
-import play.api.db.slick.Config.driver.simple._
-import play.api.data._
-import play.api.data.Forms._
 import javax.inject.Inject
-import com.mohiva.play.silhouette.core.Environment
-import com.mohiva.play.silhouette.contrib.services.CachedCookieAuthenticator
-import com.mohiva.play.silhouette.core.Silhouette
-import scala.concurrent.Future
-import play.api.Play.current
-import models.database.AdminIdentifiers
-import service.UserService
-import forms._
-import org.joda.time.format.DateTimeFormatter
-import org.joda.time.format.DateTimeFormat
-import org.joda.time.DateTimeZone
+import play.api.mvc.Controller
+import play.api.mvc.Action
 
 class AtomFeedController @Inject() (postService: PostService) extends Controller {
   
